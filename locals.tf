9cae63d31a156fd079129272af3ecfcfdead0a31
Barry Tucker <btucker@mintel.com>
2020-08-12 16:02:21 +0100

    Force ipv4 listen address

--- locals.tf
+++ locals.tf
@@ -13,7 +13,7 @@ locals {
   // Ensure max builds is optional
   runners_max_builds_string = var.runners_max_builds == 0 ? "" : format("MaxBuilds = %d", var.runners_max_builds)
 
-  metrics_listen_address = var.metrics_security_group_id == "" ? (var.metrics_public == true ? "listen_address = \":9999\"": "") : "listen_address = \":9999\""
+  metrics_listen_address = var.metrics_security_group_id == "" ? (var.metrics_public == true ? "listen_address = \"0.0.0.0:9999\"": "") : "listen_address = \":9999\""
 
   // Define key for runner token for SSM
   secure_parameter_store_runner_token_key = "${var.environment}-${var.secure_parameter_store_runner_token_key}"
